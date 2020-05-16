04a24f273f2b003448fc4e4e00c93b3155ba3fd3
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2020-05-16 22:02:41 +0200

    Add worker thread for face detection to fix video stream jittering

--- src/facedetect.py
+++ src/facedetect.py
@@ -1,12 +1,36 @@
 import cv2
+from threading import Thread
+from threading import Lock
+from queue import LifoQueue
 
 
-class FaceDetector:
+class FaceDetector(Thread):
+
     def __init__(self, classifier):
+        Thread.__init__(self)
+
         self.casc = cv2.CascadeClassifier(classifier)
+
         if self.casc.empty():
             raise Exception(f"Couldn't load classifier: {classifier}")
 
-    def detect_faces(self, image):
-        faces = self.casc.detectMultiScale(image, 1.1, 6)
+        self.daemon = True
+        self.queue = LifoQueue()
+        self.lock = Lock()
+        self.faces = []
+
+    def run(self):
+        while (True):
+            image = self.queue.get()
+            self.lock.acquire()
+            self.faces = self.casc.detectMultiScale(image, 1.1, 6)
+            self.lock.release()
+
+    def get_faces(self):
+        self.lock.acquire()
+        faces = self.faces
+        self.lock.release()
         return faces
+
+    def put_image(self, image):
+        self.queue.put(image)
--- src/main.py
+++ src/main.py
@@ -1,44 +1,49 @@
 import cv2
 import os
+import numpy as np
 from argparse import ArgumentParser
 from facedetect import FaceDetector
 
-assets_dir = os.environ["ASSETS"]
-casc = os.path.join(assets_dir, "haarcascade_frontalface_default.xml")
-detector = FaceDetector(casc)
+if __name__ == '__main__':
+    assets_dir = os.environ["ASSETS"]
+    casc = os.path.join(assets_dir, "haarcascade_frontalface_default.xml")
+    detector = FaceDetector(casc)
 
-cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0)
 
-process_each = 5
-current = 0
+    process_each = 5
+    current = 0
 
-if not cap.isOpened():
-    print("Camera open failure")
-    exit(-1)
+    if not cap.isOpened():
+        print("Camera open failure")
+        exit(-1)
 
-while True:
-    err, frame = cap.read()
+    detector.start()
 
-    if not err:
-        print(f"Did not receive frame, err: {err}")
-        break
+    while True:
+        err, frame = cap.read()
 
-    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if not err:
+            print(f"Did not receive frame, err: {err}")
+            break
 
-    if current >= process_each:
-        faces = detector.detect_faces(grayscale)
+        grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        if current >= process_each:
+            faces = detector.put_image(np.copy(grayscale))
+            current = 0
+
+        faces = detector.get_faces()
 
         for (x, y, w, h) in faces:
             cv2.rectangle(grayscale, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            
-        current = 0
 
-    cv2.imshow('Cascade Classifier', grayscale)
+        cv2.imshow('Cascade Classifier', grayscale)
 
-    if cv2.waitKey(1) == ord('q'):
-        break
+        if cv2.waitKey(1) == ord('q'):
+            break
 
-    current += 1
+        current += 1
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
